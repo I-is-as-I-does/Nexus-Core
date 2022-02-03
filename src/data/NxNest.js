efedaa2b63109f50434a38710568332328d70105
I-is-as-I-does <66915011+I-is-as-I-does@users.noreply.github.com>
2022-02-03 22:24:41 +0100

    fix nested mutation

--- src/data/NxNest.js
+++ src/data/NxNest.js
@@ -6,78 +6,75 @@ import { getSrcData } from '../load/NxSrc.js'
 import { logErr } from '../logs/NxLog.js'
 import { authorAndselectedThreadsViews } from './NxViews.js'
 
-export function buildLinkedInstances (thread, exclude = []) {
-  // @doc instances = [{url:'', ids:[]}, ...]
-  var instances = []
-  var list = []
-  thread.linked.forEach(url => {
-    if (!exclude.includes(url)) {
-      var split = splitUrlAndId(url)
-      if (!exclude.includes(split.url)) {
-        var idx = list.indexOf(split.url)
-        var id = split.id
-        if (idx !== -1) {
-          // @doc: stamper already removed duplicates
-          instances[idx].ids.push(id)
+export function buildLinkedInstances (view, exclude = []) {
+  // @doc instances = {url:[...ids] }
+  var instances = {}
+
+  for(var i=0; i < view.data.linked.length; i++){
+    if (!exclude.includes(view.data.linked[i])) {
+      var split = splitUrlAndId(view.data.linked[i])
+      if (!exclude.includes(split.url)) { 
+        if(Object.prototype.hasOwnProperty.call(instances, split.url)){
+          instances[split.url].push(split.id)
         } else {
-          instances.push({
-            url: split.url,
-            ids: [id]
-          })
-          list.push(split.url)
+          instances[split.url] = [split.id]
         }
       }
     }
-  })
+  }
   return instances
 }
 
+
 export function getLinkedInstances (view, exclude = []) {
-  var instances = getStoredLinkedMaps(view.src)
-  var register = false
-  if (instances === null) {
-    instances = buildLinkedInstances(view.data, exclude)
-    register = true
+  var store = { instances: getStoredLinkedMaps(view.src), register: false }
+  if (store.instances === null) {
+    store.instances = buildLinkedInstances(view, exclude)
+    store.register = true
   }
-  return { instances: instances, register: register }
+  return store
 }
 
-export function resolveInstanceViews (instance) {
-  var result = { views: { threads: [], author: null }, register: false }
-  return getSrcData(instance.url).then(nxdata => {
-    var viewstore = authorAndselectedThreadsViews(nxdata, instance.url, instance.ids)
+export function resolveInstanceViews (url, ids) {
+  var result = { register: false, views: [], confirmed: null}
+ return getSrcData(url).then(nxdata => {
+    var viewstore = authorAndselectedThreadsViews(nxdata, url, ids)
     if (viewstore.failed.length) {
       result.register = true
-      instance.ids = viewstore.confirmed
       logErr('Linked threads could not be resolved', viewstore.failed.join(', '))
     }
     result.views = viewstore.views
+    result.confirmed = viewstore.confirmed
     return result
   }).catch(() => {
     result.register = true
-    logErr('Linked source could not be resolved', instance.url)
+    logErr('Linked source could not be resolved', url)
     return result
   })
 }
 
+
 export function resolveLinkedViews (view, exclude = []) {
   var store = getLinkedInstances(view, exclude)
-  var promises = []
-
-  store.instances.forEach((instance) => {
-    var prom = resolveInstanceViews(instance).then(result => {
-      if (result.register) {
-        store.register = true
+  var register = store.register
+  var confirmedInstances = {}
+  const promises = []
+  for(let [url, ids] of Object.entries(store.instances)){
+    promises.push(resolveInstanceViews(url, ids).then(result => {
+      if(result.confirmed !== null){
+        confirmedInstances[url] = result.confirmed
       }
-      view.nested.push({ views: result.views })
-    })
-    promises.push(prom)
-  })
+      if(!register && result.register){
+        register = true
+      }
+      view.nested.push({views : result.views})
+    }))
+  }
+
   return Promise.all(promises).then(() => {
     view.resolved.nested = true
-
-    if (store.register) {
-      registerLinkedMaps(view.src, store.instances)
+    if (register) {
+      registerLinkedMaps(view.src, confirmedInstances)
     }
   })
 }
